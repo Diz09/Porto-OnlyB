b77fa96c130cd49a8e0a43368238a8ce52f834c4
Dimas <124556325+Diz09@users.noreply.github.com>
2025-05-28 20:44:52 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ResumeController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TestimonialController;
+use App\Http\Controllers\WorkController;
+
+// Route::get('/', function () {    return view('index');  });
+
+Route::get('/', [HomeController::class, 'show'])->name('home');
+Route::get('welcome', [WelcomeController::class, 'show'])->name('welcome');
+Route::get('about', [AboutController::class, 'show'])->name('about');
+Route::get('contact', [ContactController::class, 'show'])->name('contact');
+Route::get('resume', [ResumeController::class, 'show'])->name('resume');
+Route::get('service', [ServiceController::class, 'show'])->name('service');
+Route::get('testimonial', [TestimonialController::class, 'show'])->name('testimonial');
+Route::get('work', [WorkController::class, 'show'])->name('work');
